c757e1cd193855837af907b25a04006cc038e386
Stan Mots <stuffokator@gmail.com>
2017-04-07 13:41:47 +0300

    Add showOverSizeError and canSelectMedia PickerContract methods

--- boxing/src/main/java/com/bilibili/boxing/presenter/PickerContract.java
+++ boxing/src/main/java/com/bilibili/boxing/presenter/PickerContract.java
@@ -54,6 +54,11 @@ public interface PickerContract {
         void showAlbum(@Nullable List<AlbumEntity> albums);
 
         /**
+         * show the error message regarding the oversize media
+         */
+        void showOverSizeError();
+
+        /**
          * get the {@link ContentResolver} in the view
          */
         @NonNull
@@ -115,6 +120,14 @@ public interface PickerContract {
         boolean hasNextPage();
 
         boolean canLoadNextPage();
+
+        /**
+         * Checks whether the media is allowed to be selected in accordance with current config.
+         *
+         * @param media The media to check.
+         * @return {@code true} if the media is valid, {@code false} otherwise.
+         */
+        boolean canSelectMedia(BaseMedia media);
 
         /**
          * load next page
